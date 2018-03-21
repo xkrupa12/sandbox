b0ae8d0ca136d1fea3bca9e57675a5d7c757da42
Peter Krupa <Peter.Krupa@dixonscarphone.com>
2018-03-21 14:17:59 +0100

    Install on npm dependencies and run of laravel-mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="UTF-8">
-    <title>TITLEE!</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    {{-- STYLES --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div class="content">
